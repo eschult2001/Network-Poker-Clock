feafe5f3d57f51caf3cd212ac7932b846864e37d
Eric Schult <eschult2001@yahoo.com>
2010-11-04 09:16:37 -0400

    - Cleanup of resources

--- src/main/java/dpclock/DPClock.java
+++ src/main/java/dpclock/DPClock.java
@@ -15,6 +15,7 @@ public class DPClock {
 	 */
 	public static void main(String[] args) {
 
+		System.setProperty("java.net.preferIPv4Stack", "true");
         Launcher launcher = new Launcher();
         launcher.launch();
 	}
